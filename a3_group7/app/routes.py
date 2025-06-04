fddbd11f06f96c4af25e41985cfaac733197f12a
Anagha531 <anaghamohan531@gmail.com>
2025-06-04 13:51:21 +1000

    Delete a3_group7 directory

--- a3_group7/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template 
-from yourapp.models import Event 
-
-@app.route('/events/<int:event_id>')
-def event_detail(event_id):
-  event = Event.query.get_or_404(event_id)
-  return render_template('event_details.html',event=event)
